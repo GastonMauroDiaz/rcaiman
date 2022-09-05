0f4c5b7e90eb43d87f0c59ce76e53905ffcfde53
Gaston Diaz <gdiaz@ciefap.org.ar>
2022-09-05 16:00:44 -0300

    fix qtree()

--- R/qtree.R
+++ R/qtree.R
@@ -1,5 +1,7 @@
 qtree <- function(r, scale_parameter = 0.2) {
   stopifnot(class(r) == "SpatRaster")
+  stopifnot(is.numeric(scale_parameter))
+  stopifnot(length(scale_parameter) == 1)
 
   x <- round(min(ncol(r), nrow(r))/4)
   y <- paste0(c(1, rep(0, (nchar(x)-1))), collapse = "") %>% as.numeric()
@@ -9,17 +11,23 @@ qtree <- function(r, scale_parameter = 0.2) {
   wds <- x / 2^(1:10)
   wds <- wds[wds > 2^2]
   wds <- wds[.is_whole(wds)]
+  if (length(wds) > 5) wds <- wds[1:5]
   if (length(wds) < 1) stop("A greater number of pixels is required")
 
-  .r <- terra::subset(r, 1)
-  .ges <- Map(chessboard, .r, wds)
-  m <- Map(function(i) extract_feature(.r, .ges[[i]], length) == wds[[i]]^2,
-           seq_along(wds))
-  rm(.r)
-  ges <- Map(function(i) .ges[[i]] * m[[i]], seq_along(wds))
+  e <- terra::ext(0, ((trunc(ncol(r)/wds[1])) + 1) * wds[1],
+                  0, ((trunc(nrow(r)/wds[1])) + 1) * wds[1])
+  .r <- terra::extend(r, e)
+
+  ges <- Map(chessboard, .r, wds)
+  names(ges) <- NULL
 
   .calc_delta_single_layer <- function(r) {
-    sd_now <- Map(function(g) extract_feature(r, g, sd,
+    .sd <- function(x) {
+      x <- sd(x, na.rm = TRUE)
+      if (is.na(x)) x <- 0
+      x
+    }
+    sd_now <- Map(function(g) extract_feature(r, g, .sd,
                                               return_raster = FALSE),
                   ges)
     indices_if_split <- Map(function(i) extract_feature(ges[[i-1]], ges[[i]],
@@ -35,35 +43,73 @@ qtree <- function(r, scale_parameter = 0.2) {
   }
 
   if (terra::nlyr(r) > 1) {
-    delta <- Map(.calc_delta_single_layer, as.list(r)) %>% suppressWarnings()
+    delta <- Map(.calc_delta_single_layer, as.list(.r))
     delta <- Map(function(i) {
       x <- Map(function(j) delta[[j]][[i]], seq_along(delta))
       apply(as.data.frame(x), 1, mean)
     }, seq_along(delta[[1]]))
   } else {
-    delta <- .calc_delta_single_layer(r) %>% suppressWarnings()
+    delta <- .calc_delta_single_layer(.r)
   }
-  it_should_be_splited <- Map(function(x) x > scale_parameter, delta)
-  it_should_be_splited <- Map(function(i) {
-    terra::subst(ges[[i]],
-                 names(delta[[i]]) %>%
-                   as.numeric(),
-                 it_should_be_splited[[i]])
-  }, seq_along(it_should_be_splited))
-  ges <- ges[-1]
-  ges <- Map(function(i) {
-    r <- ges[[i]]
-    r[r == 0] <- NA
-    r + i*10000000
-  }, seq_along(ges))
-  ges <- terra::rast(ges)
-  it_should_be_splited <- terra::rast(it_should_be_splited)
-  seg <- ges * it_should_be_splited
+
+  .it_should_be_splited <- function(delta) {
+    it_should_be_splited <- Map(function(x) x > scale_parameter, delta)
+    it_should_be_splited <- Map(function(i) {
+      terra::subst(ges[[i]],
+                   names(delta[[i]]) %>%
+                     as.numeric(),
+                   it_should_be_splited[[i]])
+    }, seq_along(it_should_be_splited))
+    ges <- ges[-1]
+    cte <- rep(0, nchar(max(terra::rast(ges)[]))) %>% c(1, .) %>%
+      paste0(., collapse = "") %>% as.numeric()
+    ges <- Map(function(i) {
+      r <- ges[[i]]
+      r[r == 0] <- NA
+      r + i*cte
+    }, seq_along(ges))
+    ges <- terra::rast(ges)
+    it_should_be_splited <- terra::rast(it_should_be_splited)
+    seg <- ges * it_should_be_splited
+    seg <- max(seg)
+    size <- extract_feature(seg, seg, length)
+    i <- unique(size[])
+    i <- i[!(sqrt(i) %in% wds)]
+    i <- i[!is.na(i)]
+    if (length(i) > 0) {
+      for (u in i) {
+        seg[size == u] <- 0
+      }
+    }
+    seg
+  }
+  seg <- .it_should_be_splited(delta)
+  m <- seg == 0
+  foo <- terra::rast(ges) * m
+  cte <- paste0(c(1, rep(0, nchar(max(foo[])))), collapse = "") %>% as.numeric()
+  foo <- Map(function(i) {
+               m <- extract_feature(foo[[i]], foo[[i]], length) == wds[[i]]^2
+               m * (length(wds) - i)
+             },
+             seq_along(wds))
+  foo <- length(ges) - max(terra::rast(foo), na.rm = TRUE)
+  foo[is.na(foo)] <- 0
+  for (i in seq_along(ges)){
+    indices <- foo == i
+    if (any(indices[] %>% as.logical())) {
+      if (i == 1) {
+        foo[indices] <- ges[[1]][indices] + length(ges)
+      } else {
+        foo[indices] <- ges[[i]][indices] + fact
+      }
+      fact <- max(ges[[i]][indices])
+    }
+  }
+  foo <- foo + max(seg[])
+  seg <- c(foo*m, seg)
   seg <- max(seg)
-  seg[seg == 0] <- NA
-  middle <- length(wds) / 2
-  middle <- round(middle)
-  seg <- terra::cover(seg, .ges[[middle]])
+
+  seg <- terra::crop(seg, r)
   names(seg) <- "Quad-tree"
   seg
 }
